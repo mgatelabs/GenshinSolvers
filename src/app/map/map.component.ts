d188f2c17c29d6948a45e831f5a9890e099cf8f6
Michael Fuller <developer@mgatelabs.com>
2022-02-23 21:18:15 -0600

    1. Adding images 2. Added background color 3. It is loading in the teleport points

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -2,6 +2,7 @@ import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import { DataSourceService } from '../data-source.service';
 import { LocationInfo } from '../location-info';
+import { LocationType } from '../location-type';
 
 @Component({
   selector: 'app-map',
@@ -12,31 +13,59 @@ export class MapComponent implements AfterViewInit {
   private map: any;
 
   private initMap(): void {
-    /*
-    this.map = L.map('map', {
-      center: [39.8282, -98.5795],
-      zoom: 3,
+    let mapIcon = new L.Icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'leaflet/marker-icon.png',
+      shadowUrl: 'leaflet/marker-shadow.png',
     });
-    */
+
+    let wayPointIcon = new L.Icon({
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      iconUrl: 'assets/waypoint.png',
+      shadowUrl: 'leaflet/marker-shadow.png',
+    });
 
     this.map = L.map('map', {
       crs: L.CRS.Simple,
       minZoom: -5,
     });
 
+    let map_width = 5350;
+    let map_x = -545;
+    let map_y = -4025;
+
     var bounds: L.LatLngBounds = new L.LatLngBounds(
-      L.latLng([-3000, -550]),
-      L.latLng([-9500.5, 5023])
+      L.latLng([map_y, map_x]),
+      L.latLng([map_y - map_width, map_x + map_width])
     );
 
-    var image = L.imageOverlay('uqm_map_full.png', bounds).addTo(this.map);
+    var backgrondImage = L.imageOverlay(
+      './assets/inazuma-map.png',
+      bounds
+    ).addTo(this.map);
 
     this.map.setView([-6570, 1520], -3);
 
     for (let i = 0; i < this.dataSource.locations.length; i++) {
       let info: LocationInfo = this.dataSource.locations[i];
       var sol = L.latLng([-info.y, info.x]);
-      L.marker(sol).addTo(this.map);
+
+      let ic: L.Icon;
+
+      switch (this.dataSource.locations[i].type) {
+        case LocationType.WAYPOINT:
+          {
+            ic = wayPointIcon;
+          }
+          break;
+        default:
+          ic = mapIcon;
+          break;
+      }
+
+      L.marker(sol, { icon: ic }).addTo(this.map);
     }
   }
 

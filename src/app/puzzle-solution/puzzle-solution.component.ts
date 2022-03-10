baa055721379818ff2d7dc703e16866b98f85637
Michael Fuller <developer@mgatelabs.com>
2022-03-09 19:41:02 -0600

    Fixing display issues

--- src/app/puzzle-solution/puzzle-solution.component.ts
+++ src/app/puzzle-solution/puzzle-solution.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { PuzzleInfo } from '../shared/puzzle-info';
 import { PuzzleType } from '../shared/puzzle-type';
 import { PuzzleSolveStep } from '../solver/puzzle';
@@ -8,7 +16,7 @@ import { PuzzleSolveStep } from '../solver/puzzle';
   templateUrl: './puzzle-solution.component.html',
   styleUrls: ['./puzzle-solution.component.scss'],
 })
-export class PuzzleSolutionComponent implements OnInit {
+export class PuzzleSolutionComponent implements OnInit, OnChanges {
   @Input()
   public puzzleSolution: Array<PuzzleSolveStep> | undefined;
 
@@ -16,6 +24,8 @@ export class PuzzleSolutionComponent implements OnInit {
   public puzzleInfo: PuzzleInfo | undefined;
 
   public readonly badType: PuzzleType = PuzzleType.BROKEN;
+
+  public selectedIndex: number = -1;
 
   @Output()
   showSolution: EventEmitter<PuzzleSolveStep> = new EventEmitter<PuzzleSolveStep>();
@@ -28,10 +38,18 @@ export class PuzzleSolutionComponent implements OnInit {
   ngOnInit(): void {}
 
   showStep(index: number) {
+    this.selectedIndex = index;
     this.showSolution.emit(this.puzzleSolution![index]);
   }
 
   showFinalAction() {
+    this.selectedIndex = -2;
     this.showFinal.emit(this.puzzleSolution![this.puzzleSolution!.length - 1]);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if ((changes && changes['puzzleSolution']) || changes['puzzleInfo']) {
+      this.selectedIndex = -1;
+    }
   }
 }
